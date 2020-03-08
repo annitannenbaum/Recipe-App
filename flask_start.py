3467f9f3114e1f46447a2226204357dfc15fef3a
Anni Hoege <annahoege@gmx.de>
2020-03-08 14:46:29 +0100

    Form Update

--- flask_start.py
+++ flask_start.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "xIdpCfOFfE"
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', title="Home")
 
 @app.route('/recipes')
 def recipes():
-    return render_template('recipes.html')
+    return render_template('recipes.html', title="Recipes")
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
+@app.route('/register')
+def register():
+    form = RegistrationForm
+    return render_template('register.html', title="Register", form= form)
 
 
 # If File is run directly, the app runs in debug mode
